1f06bce77a34860ff89bb9b2c54d254d8934aa9d
Roy Lines <roy@roylines.co.uk>
2012-10-05 06:37:22 +0100

    added some server tests

--- test/server_test.js
+++ test/server_test.js
@@ -4,9 +4,37 @@ var assert = require('assert'),
       lear = require('../lib/lear.js');
 
 describe('server', function() {
-  it('should bind app',  function() {
-    assert.notEqual(server.app, null);
-    assert.notEqual(server.app, undefined);
+  describe('with no mocks', function() {
+    it('should bind app',  function() {
+      assert.notEqual(server.app, null);
+      assert.notEqual(server.app, undefined);
+    });
+    describe('port', function() {
+      describe('with PORT set', function() {
+        var oldPort = process.env.PORT;
+        before(function() {
+          process.env.PORT = 42;
+        })
+        after(function() {
+          process.env.PORT = oldPort;
+        })
+        it('should use PORT', function() {
+          assert.equal(server.port(), 42);
+        })
+      });
+      // describe('without PORT set', function() {
+      //   var oldPort = process.env.PORT;
+      //   before(function() {
+      //     process.env.PORT = null;
+      //   })
+      //   after(function() {
+      //     process.env.PORT = oldPort;
+      //   })
+      //   it('should use PORT', function() {
+      //     assert.equal(server.port(), 5000);
+      //   })
+      // });
+    });
   });
   describe('with app mock', function() {
     var oldApp = server.app;
